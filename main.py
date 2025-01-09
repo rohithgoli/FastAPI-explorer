4ce067d0c5c635f0c9b39ed01f18c6f31958acb5
Rohith Goli <rohithgoli396@gmail.com>
2025-01-09 07:00:02 +0530

    feat: explored and added Path Parameters

--- main.py
+++ main.py
@@ -3,19 +3,33 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-@app.get('/')
-def index():
+@app.get('/')   # path operation decorator (path & operator)
+def index():    # path operation function
     return {
         "data": {
-            "greeting": "Hello"
+            "greeting": "blog list"
         }
     }
 
 
-@app.get('/about')
-def about():
+@app.get('/blog/unpublished')
+def unpublished():
     return {
-        "data": {
-            "about page"
-        }
+        'data': 'all unpublished blogs'
+    }
+
+
+@app.get('/blog/{id}')
+def show_blog(id: int):
+    #  fetch blog with id = id
+    return {
+        'data': id
+    }
+
+
+@app.get('/blog/{id}/comments')
+def comments(id):
+    # fetch comments of blog with id = id
+    return {
+        'data': ['1', '2']
     }
