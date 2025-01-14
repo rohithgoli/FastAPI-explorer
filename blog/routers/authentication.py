058c16069417dddb0e9d19891b12de27a6e470d4
Rohith Goli <rohithgoli396@gmail.com>
2025-01-15 00:50:33 +0530

    feat: added authentication to api

--- blog/routers/authentication.py
+++ blog/routers/authentication.py
@@ -1,5 +1,6 @@
 
 from fastapi import APIRouter, Depends, status, HTTPException
+from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 from blog import schemas, database, models
 from blog.hashing import Hash
@@ -12,7 +13,7 @@ router = APIRouter(
 
 
 @router.post('/login')
-def login(request: schemas.Login, db: Session = Depends(database.get_db)):
+def login(request: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(database.get_db)):
     user = db.query(models.User).filter(models.User.email == request.username).first()
     if not user:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
